37715aa46395619a24c94f5b15130192469c24de
gp <greg.parsons@inclimesolutions.com>
2022-11-14 19:10:21 -0800

    go insecure

--- src/main.rs
+++ src/main.rs
@@ -15,30 +15,43 @@ fn main() {
         .expect("Failed building the Runtime");
 
     let _ = rt.block_on(
-        run_stuff()
+        send_email("subject here",
+                   "<h1>Hello, world!</h1>",
+                   "Hello world!",
+                   "from_me@example.com",
+                   "From Me",
+                   vec![
+                       ("Jane Doe".to_string(), "jane@example.com".to_string()),
+                       ("James Smith".to_string(), "james@test.com".to_string()),
+                   ]
+        )
     );
-
 }
 
-async fn run_stuff(){
+async fn send_email(
+    subject:&str, html_body:&str, text_body:&str,
+    from_email:&str, from_name:&str,
+    recipient_list:Vec<(String, String)>
+){
 
     let server = env::var("SMTP_SERVER").unwrap();
+    // let username = env::var("SMTP_USER").unwrap();
+    // let password = env::var("SMTP_PW").unwrap();
 
     let message = MessageBuilder::new()
-        .from(("John Doe", "john@example.com"))
-        .to(vec![
-            ("Jane Doe", "jane@example.com"),
-            ("James Smith", "james@test.com"),
-        ])
-        .subject("Hi!")
-        .html_body("<h1>Hello, world!</h1>")
-        .text_body("Hello world!");
+        .from((from_name, from_email))
+        .to(recipient_list)
+        .subject(subject)
+        .html_body(html_body)
+        .text_body(text_body);
 
     // Connect to an SMTP relay server over TLS and
     // authenticate using the provided credentials.
     Transport::new(server)
-        .credentials("john", "p4ssw0rd")
-        .connect_tls()
+        .port(2525)
+        // .credentials(username, password)
+        // .connect_tls()
+        .connect()
         .await
         .unwrap()
         .send(message)
