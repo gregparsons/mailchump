4581564e707a6e521ecf8b38ff70518019c99a38
gp <greg.parsons@inclimesolutions.com>
2022-11-14 18:13:08 -0800

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use mail_send::mail_builder::MessageBuilder;
+use mail_send::Transport;
+use dotenvy::dotenv;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+
+    dotenv().ok();
+
+    let server = env::var("SMTP_SERVER").unwrap();
+
+    // Build a simple multipart message
+    let message = MessageBuilder::new()
+        .from(("John Doe", "john@example.com"))
+        .to(vec![
+            ("Jane Doe", "jane@example.com"),
+            ("James Smith", "james@test.com"),
+        ])
+        .subject("Hi!")
+        .html_body("<h1>Hello, world!</h1>")
+        .text_body("Hello world!");
+
+    // Connect to an SMTP relay server over TLS and
+    // authenticate using the provided credentials.
+    Transport::new(server)
+        .credentials("john", "p4ssw0rd")
+        .connect_tls()
+        .await
+        .unwrap()
+        .send(message)
+        .await
+        .unwrap();
+
+
 }
